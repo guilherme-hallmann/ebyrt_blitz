d16fdea422779b55b093c8201d43f9d91e5d5417
Guilherme <guilhermehallmann@gmail.com>
2022-07-04 13:52:03 -0300

    feat: creating userService and user route

--- app/backend/src/controllers/userController.js
+++ app/backend/src/controllers/userController.js
@@ -1,15 +1,11 @@
 const userService = require('../services/userService');
 
-const create = async (req, res, next) => {
-  try {
-    const {
-      name, email, password,
-    } = req.body;
-    const token = await userService.create(name, email, password);
-    return res.status(201).json({ token });
-  } catch (err) {
-    next(err);
-  }
+const create = async (req, res) => {
+  const {
+    name, email, password,
+  } = req.body;
+  const token = await userService.create(name, email, password);
+  return res.status(201).json({ token });
 };
 
 module.exports = {
--- app/backend/src/routes/index.js
+++ app/backend/src/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const loginRoute = require('./login/index');
 
 const router = express.Router();
+router.use('/login', loginRoute);
 
 module.exports = router;
