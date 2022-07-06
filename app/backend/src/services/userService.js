e120e8c959c24c99a68cad72811a964d5909b47e
Guilherme <guilhermehallmann@gmail.com>
2022-07-06 09:59:24 -0300

    fix: changing userService to get password with hash

--- app/backend/src/services/userService.js
+++ app/backend/src/services/userService.js
@@ -3,11 +3,12 @@ const errorThrow = require('../utils/errorThrow');
 const generateToken = require('../utils/generateJWT');
 const { hashPw } = require('../utils/passwordHash');
 
-const create = async ({ name, email, password }) => {
+const create = async (data) => {
+  const { name, email } = data;
   const user = await User.findOne({ where: { email } });
   if (user) throw errorThrow(409, 'User already exists!');
-  const encryptedPw = hashPw(password);
-  const { id } = await User.create({ name, email, encryptedPw });
+  const password = hashPw(data.password);
+  const { id } = await User.create({ name, email, password });
   const token = generateToken({ id, name, email });
   return token;
 };
