247d888df72cd0d8c6797c1df500aadba7e1d68c
Guilherme <guilhermehallmann@gmail.com>
2022-07-06 10:05:33 -0300

    feat: creating login service

--- app/backend/src/services/loginService.js
+++ app/backend/src/services/loginService.js
@@ -1,8 +1,24 @@
 const { User } = require('../database/models');
 const errorThrow = require('../utils/errorThrow');
 const generateToken = require('../utils/generateJWT');
+const { checkHash } = require('../utils/passwordHash');
 
 const login = async ({ email, password }) => {
   const user = await User.findOne({ where: { email } });
-  if (!user) throw errorThrow(409, 'User not found!');
+  if (!user) throw errorThrow(404, 'User not found!');
+  const pwValidation = await checkHash(password, user.password);
+  if (!pwValidation) throw errorThrow(401, 'Invalid password!');
+  const token = generateToken({ id: user.id, name: user.name, email: user.email });
+  return {
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token,
+    },
+  };
+};
+
+module.exports = {
+  login,
 };
