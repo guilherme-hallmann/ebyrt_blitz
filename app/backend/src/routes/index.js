b630ea4432d45b8454c53f0ef633a502d8a1b684
Guilherme <guilhermehallmann@gmail.com>
2022-07-06 10:08:17 -0300

    feat: importing login route to route index

--- app/backend/src/routes/index.js
+++ app/backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userRoute = require('./user');
+const loginRoute = require('./login');
 
 const router = express.Router();
 router.use('/user', userRoute);
+router.use('/login', loginRoute);
 
 module.exports = router;
