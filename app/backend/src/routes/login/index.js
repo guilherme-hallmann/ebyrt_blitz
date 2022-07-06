a625896f0c324466f4c36ec8bd49bc616b9984ee
Guilherme <guilhermehallmann@gmail.com>
2022-07-06 10:07:12 -0300

    feat: creating login data validation

--- app/backend/src/routes/login/index.js
+++ app/backend/src/routes/login/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const loginController = require('../../controllers/loginController');
+const loginValidation = require('../../joi/loginSchema');
 
 const router = express.Router();
 
-router.post('/', loginController.login);
+router.post('/', loginValidation, loginController.login);
 
 module.exports = router;
